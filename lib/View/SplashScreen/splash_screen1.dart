a0407fb31563df4226dd98578f231177927b6aab
Your Name <you@example.com>
2024-06-04 23:19:33 +0600

    Splash screen added

---
+++ lib/View/SplashScreen/splash_screen1.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SplashScreen1 extends StatefulWidget {
+  const SplashScreen1({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen1> createState() => _SplashScreen1State();
+}
+
+class _SplashScreen1State extends State<SplashScreen1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("SplashScreen 1",style: GoogleFonts.poppins(
+          fontSize: 16,
+          fontWeight: FontWeight.w500,
+          color: Colors.red
+        ),),
+      ),
+
+    );
+  }
+}
