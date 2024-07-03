f0d61fe691e4e0b57597f9a2d2d43db299019283
jibika52 <jibikaapp@gmail.com>
2024-07-03 17:39:37 +0600

    done youtube video

--- lib/View/BootomBar/ProfileScreen/SubsCriptionScreen/subscription_screen.dart
+++ lib/View/BootomBar/ProfileScreen/SubsCriptionScreen/subscription_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:seru_test_project/CustomWidget/CustomText/custom_text.dart';
 
+import '../../../../CustomWidget/CustomImageScetion/custom_image_section.dart';
+import '../../../../custom_const.dart';
+
 class SubscriptionScreen extends StatefulWidget {
   const SubscriptionScreen({super.key});
 
@@ -9,15 +12,76 @@ class SubscriptionScreen extends StatefulWidget {
 }
 
 class _SubscriptionScreenState extends State<SubscriptionScreen> {
+  bool  is_cliced=false;
+  bool  is_cliced_for_own=true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         height: double.infinity,
         width: double.infinity,
-        color: Colors.red,
-        alignment: Alignment.center,
-        child: CustomText(text: "Subscription", fontSize: 33, fontWeight: FontWeight.bold),
+        child:  Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: SingleChildScrollView(
+            child: GridView.builder(
+              shrinkWrap: true,
+              itemCount: 3,
+              physics: NeverScrollableScrollPhysics(),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  mainAxisExtent: 190
+              ), itemBuilder: (context, index) {
+              return InkWell(
+                onTap: () {
+
+                },
+                child: Container(
+                  height: 100,
+                  padding: const EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)
+                    ,color: Main_Theme_Color,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child:  Container(
+                          height: 30,
+                          width: 100,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              color: Colors.black
+                          ),
+                          alignment: Alignment.center,
+                          child: CustomText(text: "\£ 29",text_color: main_text_white_color ,fontSize: 17, fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                      SizedBox(height: 5,),
+                      CustomText(text: "✓ 20 Mocktest", fontSize: 18, fontWeight: FontWeight.w500)
+                      ,
+                      SizedBox(height: 5,),
+                      CustomText(text: "✓ 20 Mocktest", fontSize: 16, fontWeight: FontWeight.w500)
+                      ,SizedBox(height: 5,),
+                      GestureDetector(
+                        onTap: () {
+
+                        },
+                        child: CircleAvatar(
+                          radius: 30,
+                          backgroundImage: AssetImage("assets/Gif/buynowcircle.gif"),
+                        ),
+                      ),
+                      // Image.asset("assets/Gif/buynow.webp",height: 40,width: 100,fit: BoxFit.fill,)
+                    ],
+                  ),
+                ),
+              );
+            },),
+          ),
+        ),
       ),
     );
   }
